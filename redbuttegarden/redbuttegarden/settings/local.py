6119486a774862169a081a02ea0e0f5dcb6913b2
Avery Uslaner <avery.uslaner@redbutte.utah.edu>
2020-09-19 10:31:15 -0600

    Disabled S3 Static Storage for Local Settings
    Signed-off-by: Avery Uslaner <avery.uslaner@redbutte.utah.edu>

--- redbuttegarden/redbuttegarden/settings/local.py
+++ redbuttegarden/redbuttegarden/settings/local.py
@@ -1,9 +1,18 @@
 from .dev import *
 
 # Use local static file storage when running locally
+AWS_STORAGE_BUCKET_NAME = ''
+AWS_S3_REGION_NAME = 'us-east-1'
+STATIC_BUCKET = ''
+MEDIA_BUCKET = ''
+DEFAULT_FILE_STORAGE = 'django.core.files.storage.FileSystemStorage'
+AWS_S3_CUSTOM_DOMAIN = ''
 STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
+
+AWS_ACCESS_KEY_ID = 'FOO'
+AWS_SECRET_ACCESS_KEY = 'BAR'
 
 # Disable cloudfront invalidations when running locally
 WAGTAILFRONTENDCACHE = {}
