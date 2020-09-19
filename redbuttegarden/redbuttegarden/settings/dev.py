c6b19448d5b1c5793877184f9f687d9837309f28
Avery Uslaner <avery.uslaner@redbutte.utah.edu>
2020-09-19 10:30:13 -0600

    Changed BASE_URL in dev settings
    Signed-off-by: Avery Uslaner <avery.uslaner@redbutte.utah.edu>

--- redbuttegarden/redbuttegarden/settings/dev.py
+++ redbuttegarden/redbuttegarden/settings/dev.py
@@ -12,6 +12,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: define the correct hosts in production!
 ALLOWED_HOSTS = ['0.0.0.0', 'trjxa2b547.execute-api.us-east-1.amazonaws.com', 'dhsyi82ptcyu5.cloudfront.net', 'dev.redbuttegarden.org']
 
+BASE_URL = 'https://dev.redbuttegarden.org'
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 # Database
