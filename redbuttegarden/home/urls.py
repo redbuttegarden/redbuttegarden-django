6574f97a95ced831f02075f7b037cea98bbac7b8
Avery Uslaner <avery.uslaner@redbutte.utah.edu>
2020-12-10 12:13:00 -0700

    Hardcoded Directions Page Removed
    Replaced by page created in Wagtail.
    
    Signed-off-by: Avery Uslaner <avery.uslaner@redbutte.utah.edu>

--- redbuttegarden/home/urls.py
+++ redbuttegarden/home/urls.py
@@ -4,6 +4,5 @@ from . import views
 
 app_name = 'home'
 urlpatterns = [
-    path('directions/', views.directions, name='directions'),
     path('general-info/', views.general_info, name='general-info'),
 ]
