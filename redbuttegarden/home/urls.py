513c4f00d6958daf836f475df06670586b443023
Avery Uslaner <avery.uslaner@redbutte.utah.edu>
2020-12-10 12:01:23 -0700

    Hardcoded About Us Page Removed
    Replaced by page created in Wagtail.
    
    Signed-off-by: Avery Uslaner <avery.uslaner@redbutte.utah.edu>

--- redbuttegarden/home/urls.py
+++ redbuttegarden/home/urls.py
@@ -4,7 +4,6 @@ from . import views
 
 app_name = 'home'
 urlpatterns = [
-    path('about-us/', views.about_us, name='about-us'),
     path('directions/', views.directions, name='directions'),
     path('general-info/', views.general_info, name='general-info'),
 ]
