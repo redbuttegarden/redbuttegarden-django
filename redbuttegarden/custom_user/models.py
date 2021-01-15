9660fea39e45bffd1973eae7a5d621d74d0296b9
Avery Uslaner <avery.uslaner@redbutte.utah.edu>
2021-01-15 12:16:44 -0700

    Custom User Model Updates
    Updated the __repr__ and __str__ methods of the custom user model to make it easier for users to choose an author when creating Journal pages written by someone other than themselves.
    
    Signed-off-by: Avery Uslaner <avery.uslaner@redbutte.utah.edu>

--- redbuttegarden/custom_user/models.py
+++ redbuttegarden/custom_user/models.py
@@ -4,3 +4,9 @@ from django.db import models
 
 class User(AbstractUser):
     title = models.CharField(max_length=255)
+
+    def __repr__(self):
+        return f'{self.first_name} {self.last_name} ({self.username})'
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name} ({self.username})'
