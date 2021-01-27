9c6a5dafcd487eadb0786a1b95aac37d9405db2a
Avery Uslaner <avery.uslaner@redbutte.utah.edu>
2021-01-27 11:31:18 -0700

    Added DonorPackagePage Creation Test
    Signed-off-by: Avery Uslaner <avery.uslaner@redbutte.utah.edu>

--- redbuttegarden/concerts/tests/test_models.py
+++ redbuttegarden/concerts/tests/test_models.py
@@ -1,9 +1,12 @@
 from wagtail.core.models import Page
 from wagtail.tests.utils import WagtailPageTests
 
-from concerts.models import ConcertPage
+from concerts.models import ConcertPage, DonorPackagePage
 
 
 class ConcertPageTests(WagtailPageTests):
     def test_can_create_concert(self):
         self.assertCanCreateAt(Page, ConcertPage)
+
+    def test_can_create_donor_package(self):
+        self.assertCanCreateAt(Page, DonorPackagePage)
