3602ad6d8f2f4781e2fc8413cf63fe5bbdf2968f
Avery Uslaner <avery.uslaner@redbutte.utah.edu>
2021-01-27 14:47:06 -0700

    Failing Concert Page Creation Test
    Signed-off-by: Avery Uslaner <avery.uslaner@redbutte.utah.edu>

--- redbuttegarden/concerts/tests/test_models.py
+++ redbuttegarden/concerts/tests/test_models.py
@@ -1,5 +1,7 @@
+from django.contrib.auth.models import Group
 from wagtail.core.models import Page
-from wagtail.tests.utils import WagtailPageTests
+from wagtail.tests.utils import WagtailPageTests, get_user_model
+from wagtail.tests.utils.form_data import nested_form_data, rich_text, streamfield
 
 from concerts.models import ConcertPage, DonorPackagePage
 
@@ -10,3 +12,15 @@ class ConcertPageTests(WagtailPageTests):
 
     def test_can_create_donor_package(self):
         self.assertCanCreateAt(Page, DonorPackagePage)
+
+
+class ConcertPageInstanceTests(WagtailPageTests):
+    def setUp(self):
+        self.home = Page.objects.get(slug='home')
+        self.user = get_user_model().objects.create_user('Test User', 'test@email.com', 'password')
+        self.user.groups.add(Group.objects.get(name="Moderators"))
+        self.client.force_login(self.user)
+
+    def test_can_create_concert(self):
+        self.assertCanCreate(self.home, ConcertPage,
+                             nested_form_data({'title': 'Concert Test Page'}))
