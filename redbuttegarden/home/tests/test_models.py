db5e0c7b91aba055b8b1d289672771563f288900
Avery Uslaner <avery.uslaner@redbutte.utah.edu>
2021-01-28 11:30:41 -0700

    Basic instance based page creation tests for home app page models
    Signed-off-by: Avery Uslaner <avery.uslaner@redbutte.utah.edu>

--- redbuttegarden/home/tests/test_models.py
+++ redbuttegarden/home/tests/test_models.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.models import Group
 from wagtail.core.models import Page
 from wagtail.tests.utils import WagtailPageTests, get_user_model
-from wagtail.tests.utils.form_data import nested_form_data, streamfield
+from wagtail.tests.utils.form_data import nested_form_data, streamfield, inline_formset, rich_text
 
 from home.models import FAQPage, GeneralPage, GeneralIndexPage, HomePage, PlantCollectionsPage, TwoColumnGeneralPage
 
@@ -39,4 +39,38 @@ class HomePageInstanceTests(WagtailPageTests):
              'body': streamfield([
                  ('heading', 'Testing!')
              ])}
+        ))
+
+    def test_can_create_general(self):
+        self.assertCanCreate(self.home, GeneralPage, nested_form_data(
+            {'title': 'General Test Page',
+             'body': streamfield([
+                 ('html', '<p>Testing!</p>')
+             ])}
+        ))
+
+    def test_can_create_general_index(self):
+        self.assertCanCreate(self.home, GeneralIndexPage, nested_form_data(
+            {'title': 'General Index Test Page',
+             'body': streamfield([])}
+        ))
+
+    def test_can_create_home(self):
+        self.assertCanCreate(self.home, HomePage, nested_form_data(
+            {'title': 'Home Test Page',
+             'event_slides': inline_formset([])}
+        ))
+
+    def test_can_create_plant_collections(self):
+        self.assertCanCreate(self.home, PlantCollectionsPage, nested_form_data(
+            {'title': 'Plant Collections Test Page',
+             'intro': rich_text('Testing!'),
+             'more_info_modal': rich_text('Testing!'),
+             'plant_collections': inline_formset([])}
+        ))
+
+    def test_can_create_two_col_general(self):
+        self.assertCanCreate(self.home, TwoColumnGeneralPage, nested_form_data(
+            {'title': 'Two Column General Test Page',
+             'body': streamfield([])}
         ))
