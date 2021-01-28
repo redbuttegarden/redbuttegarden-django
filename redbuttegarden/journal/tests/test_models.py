774830dd2f7c13f02e9eb60610ab475cf641ec4e
Avery Uslaner <avery.uslaner@redbutte.utah.edu>
2021-01-28 12:28:09 -0700

    Basic instance based page creation tests for journal app page models
    Removed unused tests.py file
    
    Signed-off-by: Avery Uslaner <avery.uslaner@redbutte.utah.edu>

--- redbuttegarden/journal/tests/test_models.py
+++ redbuttegarden/journal/tests/test_models.py
@@ -1,5 +1,9 @@
+import json
+from django.contrib.auth.models import Group
+from django.utils import timezone
 from wagtail.core.models import Page
-from wagtail.tests.utils import WagtailPageTests
+from wagtail.tests.utils import WagtailPageTests, get_user_model
+from wagtail.tests.utils.form_data import nested_form_data, streamfield, inline_formset
 
 from journal.models import JournalPage, JournalIndexPage
 
@@ -13,3 +17,34 @@ class JournalPageTests(WagtailPageTests):
 
     def test_can_create_journal_index(self):
         self.assertCanCreateAt(Page, JournalIndexPage)
+
+
+class JournalInstanceTests(WagtailPageTests):
+    def setUp(self):
+        self.home = Page.objects.get(slug='home')
+        self.user = get_user_model().objects.create_user('Test User', 'test@email.com', 'password')
+        self.user.groups.add(Group.objects.get(name="Moderators"))
+        self.client.force_login(self.user)
+
+        try:
+            self.journal_index = JournalIndexPage(title='Journal Index Test Page',
+                                                  body=json.dumps([]))
+            self.home.add_child(instance=self.journal_index)
+        except:
+            pass
+
+    def test_can_create_journal(self):
+        self.assertCanCreate(self.journal_index, JournalPage, nested_form_data(
+            {'title': 'Journal Test Page',
+             'date': '2006-10-25 14:30:59',
+             'body': streamfield([
+                 ('green_heading', 'Testing!'),
+             ]),
+             'gallery_images': inline_formset([])}
+        ))
+
+    def test_can_create_journal_index(self):
+        self.assertCanCreate(self.home, JournalIndexPage, nested_form_data(
+            {'title': 'Another Journal Index Test Page',
+             'body': streamfield([])}
+        ))
