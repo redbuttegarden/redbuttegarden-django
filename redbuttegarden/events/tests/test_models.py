8400242b384493664d5b6c075ed998ebdc75d5f1
Avery Uslaner <avery.uslaner@redbutte.utah.edu>
2021-01-28 10:16:44 -0700

    Instance Creation Tests for Event Models
    Basic instanced page creation tests created for EventIndexPage, EventPage, and EventGeneralPage models.
    
    Signed-off-by: Avery Uslaner <avery.uslaner@redbutte.utah.edu>

--- redbuttegarden/events/tests/test_models.py
+++ redbuttegarden/events/tests/test_models.py
@@ -1,5 +1,8 @@
+import json
+from django.contrib.auth.models import Group
 from wagtail.core.models import Page
-from wagtail.tests.utils import WagtailPageTests
+from wagtail.tests.utils import WagtailPageTests, get_user_model
+from wagtail.tests.utils.form_data import nested_form_data, streamfield
 
 from events.models import EventIndexPage, EventPage, EventGeneralPage
 
@@ -19,3 +22,50 @@ class EventPageTests(WagtailPageTests):
         EventGeneral pages can only be created under EventIndexPages or GeneralIndexPages
         """
         self.assertCanNotCreateAt(Page, EventGeneralPage)
+
+
+class EventPageInstanceTests(WagtailPageTests):
+    def setUp(self):
+        self.home = Page.objects.get(slug='home')
+        self.user = get_user_model().objects.create_user('Test User', 'test@email.com', 'password')
+        self.user.groups.add(Group.objects.get(name="Moderators"))
+        self.client.force_login(self.user)
+
+        try:
+            self.event_index = EventIndexPage(title='Event Index Test Page',
+                                              body=json.dumps([
+                                                  {u'type': u'html', u'value': '<p>Testing!</p>'}
+                                              ]))
+            self.home.add_child(instance=self.event_index)
+        except:
+            raise
+
+    def test_can_create_event_index(self):
+        self.assertCanCreate(self.home, EventIndexPage,
+                             nested_form_data(
+                                 {'title': 'Event Index Test Page',
+                                  'body': streamfield([])  # Need to pass empty list to avoid ValidationError
+                                  }
+                             ))
+
+    def test_can_create_event(self):
+        self.assertCanCreate(self.event_index, EventPage,
+                             nested_form_data(
+                                 {'title': 'Event Test Page',
+                                  'location': 'Test Location',
+                                  'event_dates': 'January',
+                                  'body': streamfield([
+                                      ('html', '<p>Testing!</p>')
+                                  ])}
+                             ))
+
+    def test_can_create_event_general(self):
+        self.assertCanCreate(self.event_index, EventGeneralPage,
+                             nested_form_data(
+                                 {'title': 'Event General Test Page',
+                                  'event_dates': 'January',
+                                  'body': streamfield([
+                                      ('html', '<p>Testing!</p>')
+                                  ])
+                                  }
+                             ))
