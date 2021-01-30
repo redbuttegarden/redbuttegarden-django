7eee29ef5ce959e327c33c115aceaeeb8d251195
Avery Uslaner <avery.uslaner@redbutte.utah.edu>
2021-01-30 15:13:02 -0700

    MultiColumnAlignedParagraphBlock 3/4/5 Paragraph View Test
    Signed-off-by: Avery Uslaner <avery.uslaner@redbutte.utah.edu>

--- redbuttegarden/home/tests/test_blocks.py
+++ redbuttegarden/home/tests/test_blocks.py
@@ -51,3 +51,80 @@ class TestMultiColumnAlignedParagraphBlock(WagtailPageTests):
         self.assertIn('<div class="col-sm-6">\n                        <p>Testing!</p>\n                    </div>\n'
                       '                \n                    <div class="col-sm-6">\n                        <p>Second '
                       'Test Paragraph</p>\n                    </div>', html)
+
+    def test_view_three_paragraphs(self):
+        general_page = GeneralPage(owner=self.user,
+                                   slug='general-test-page',
+                                   title='General Test Page',
+                                   body=json.dumps([
+                                       {'type': 'multi_column_paragraph', 'value': {
+                                           'alignment': 'left',
+                                           'background_color': 'default',
+                                           'paragraph': ['<p>Testing!</p>', '<p>Second Test Paragraph</p>',
+                                                         '<p>Third Test Paragraph</p>']
+                                       }}
+                                   ]))
+        Page.objects.get(slug='home').add_child(instance=general_page)
+        general_page.save_revision().publish()
+
+        response = self.client.get('/general-test-page', follow=True)
+        html = response.content.decode('utf8')
+        self.assertIn('<div class="col-sm-4">\n                        <p>Testing!</p>\n                    </div>\n   '
+                      '             \n                    <div class="col-sm-4">\n                        <p>Second '
+                      'Test Paragraph</p>\n                    </div>\n                \n                    '
+                      '<div class="col-sm-4">\n                        <p>Third Test Paragraph</p>\n                   '
+                      ' </div>',
+                      html)
+
+    def test_view_four_paragraphs(self):
+        general_page = GeneralPage(owner=self.user,
+                                   slug='general-test-page',
+                                   title='General Test Page',
+                                   body=json.dumps([
+                                       {'type': 'multi_column_paragraph', 'value': {
+                                           'alignment': 'left',
+                                           'background_color': 'default',
+                                           'paragraph': ['<p>Testing!</p>', '<p>Second Test Paragraph</p>',
+                                                         '<p>Third Test Paragraph</p>', '<p>Fourth Test Paragraph</p>']
+                                       }}
+                                   ]))
+        Page.objects.get(slug='home').add_child(instance=general_page)
+        general_page.save_revision().publish()
+
+        response = self.client.get('/general-test-page', follow=True)
+        html = response.content.decode('utf8')
+        self.assertIn('<div class="col-sm-3">\n                        <p>Testing!</p>\n                    </div>\n   '
+                      '             \n                    <div class="col-sm-3">\n                        <p>Second '
+                      'Test Paragraph</p>\n                    </div>\n                \n                    <div '
+                      'class="col-sm-3">\n                        <p>Third Test Paragraph</p>\n                    '
+                      '</div>\n                \n                    <div class="col-sm-3">\n                        '
+                      '<p>Fourth Test Paragraph</p>\n                    </div>',
+                      html)
+
+    def test_view_five_paragraphs(self):
+        general_page = GeneralPage(owner=self.user,
+                                   slug='general-test-page',
+                                   title='General Test Page',
+                                   body=json.dumps([
+                                       {'type': 'multi_column_paragraph', 'value': {
+                                           'alignment': 'left',
+                                           'background_color': 'default',
+                                           'paragraph': ['<p>Testing!</p>', '<p>Second Test Paragraph</p>',
+                                                         '<p>Third Test Paragraph</p>', '<p>Fourth Test Paragraph</p>',
+                                                         '<p>Fifth Test Paragraph</p>']
+                                       }}
+                                   ]))
+        Page.objects.get(slug='home').add_child(instance=general_page)
+        general_page.save_revision().publish()
+
+        response = self.client.get('/general-test-page', follow=True)
+        html = response.content.decode('utf8')
+        self.assertIn('<div class="col-sm-2">\n                        <p>Testing!</p>\n                    </div>\n   '
+                      '             \n                    <div class="col-sm-2">\n                        <p>Second '
+                      'Test Paragraph</p>\n                    </div>\n                \n                    '
+                      '<div class="col-sm-2">\n                        <p>Third Test Paragraph</p>\n                   '
+                      ' </div>\n                \n                    <div class="col-sm-2">\n                        '
+                      '<p>Fourth Test Paragraph</p>\n                    </div>\n                \n                    '
+                      '<div class="col-sm-2">\n                        <p>Fifth Test Paragraph</p>\n                   '
+                      ' </div>',
+                      html)
