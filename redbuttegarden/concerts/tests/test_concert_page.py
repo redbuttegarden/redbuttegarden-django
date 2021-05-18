00f7b8f5e7f614a15c3dd643e06d84569eef5512
Avery Uslaner <avery.uslaner@redbutte.utah.edu>
2021-05-18 14:22:23 -0600

    Test Concert TBD and On Sale Functionality
    Signed-off-by: Avery Uslaner <avery.uslaner@redbutte.utah.edu>

--- redbuttegarden/concerts/tests/test_concert_page.py
+++ redbuttegarden/concerts/tests/test_concert_page.py
@@ -341,3 +341,69 @@ class TestConcert(TestCase):
         self.assertContains(response, "Show at")
         self.assertContains(response, "Band Info Test")
         self.assertNotContains(response, "Hidden Band Info Test")
+
+    def test_concert_not_on_sale(self):
+        """
+        If on_sale is False, buy button should use different alt tag
+        and be surrounded by a div rather than an anchor tag.
+        """
+        self.concert_page.body = json.dumps([
+            {"type": "concerts",
+             "value": {"band_img": 773,
+                       "hidden": False,
+                       "on_sale": False,
+                       "virtual": False,
+                       "canceled": False,
+                       "postponed": False,
+                       "sold_out": False,
+                       "available_until": None,
+                       "band_info": "<h4><b>Band Info Test</b></h4>",
+                       "concert_dates": ["2021-03-11T19:00:00-07:00",
+                                         "2021-03-12T14:00:00-07:00"],
+                       "gates_time": "14:00:00",
+                       "show_time": "19:00:00",
+                       "member_price": "BandsInTown Plus Subscription",
+                       "public_price": "BandsInTown Plus Subscription",
+                       "ticket_url": "https://www.awin1.com/cread.php?awinmid=19610&awinaffid=846015&ued="
+                       }}
+        ])
+        self.concert_page.save_revision().publish()
+
+        response = self.client.get('/concert-test-page', follow=True)
+        # Looks for alt tags of buy button used in the template
+        self.assertContains(response, 'alt="Not yet on sale"')
+        self.assertContains(response, '<div class="disable-buy">')
+        self.assertNotContains(response, 'alt="Buy ticket button"')
+        self.assertNotContains(response, '<img class="responsive con-button hover"')
+
+    def test_concert_times_tbd(self):
+        """
+        When Show Time and Gate Time are left blank, a message
+        indicating times are still to be determined should be
+        displayed in their place.
+        """
+        self.concert_page.body = json.dumps([
+            {"type": "concerts",
+             "value": {"band_img": 773,
+                       "hidden": False,
+                       "on_sale": True,
+                       "virtual": False,
+                       "canceled": False,
+                       "postponed": False,
+                       "sold_out": False,
+                       "available_until": None,
+                       "band_info": "<h4><b>Band Info Test</b></h4>",
+                       "concert_dates": ["2021-03-11T19:00:00-07:00",
+                                         "2021-03-12T14:00:00-07:00"],
+                       "gates_time": None,
+                       "show_time": None,
+                       "member_price": "BandsInTown Plus Subscription",
+                       "public_price": "BandsInTown Plus Subscription",
+                       "ticket_url": "https://www.awin1.com/cread.php?awinmid=19610&awinaffid=846015&ued="
+                       }}
+        ])
+        self.concert_page.save_revision().publish()
+
+        response = self.client.get('/concert-test-page', follow=True)
+        # Looks for alt tags of buy button used in the template
+        self.assertContains(response, "Gate & Show Times TBD")
