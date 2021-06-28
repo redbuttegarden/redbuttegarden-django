e4af5ec46b710a70b1d65a688d9bd6fefa1c1d6b
Avery Uslaner <avery.uslaner@redbutte.utah.edu>
2021-06-28 14:05:47 -0600

    DRF Serializer for Family Model
    Signed-off-by: Avery Uslaner <avery.uslaner@redbutte.utah.edu>

---
+++ redbuttegarden/plants/serializers.py
@@ -0,0 +1,16 @@
+from rest_framework import serializers
+
+from plants.models import Family
+
+
+class FamilySerializer(serializers.Serializer):
+    id = serializers.IntegerField(read_only=True)
+    name = serializers.CharField(max_length=255)
+
+    def create(self, validated_data):
+        return Family.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        instance.name = validated_data.get('name', instance.name)
+        instance.save()
+        return instance
