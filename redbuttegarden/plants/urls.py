eae1def1ae0d537aacd03b5213e0c6fdd1a045f6
Avery Uslaner <avery.uslaner@redbutte.utah.edu>
2021-06-29 11:28:55 -0600

    Refactors collection list and detail views as classed-based views
    Signed-off-by: Avery Uslaner <avery.uslaner@redbutte.utah.edu>

--- redbuttegarden/plants/urls.py
+++ redbuttegarden/plants/urls.py
@@ -3,8 +3,8 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from plants import views
 
 urlpatterns = format_suffix_patterns([
-    path('collections/', views.collection_list),
-    path('collections/<int:pk>/', views.collection_detail),
+    path('collections/', views.CollectionList.as_view()),
+    path('collections/<int:pk>/', views.CollectionDetail.as_view()),
     path('family/<int:pk>/',
          views.FamilyDetail.as_view(),
          name='family-detail'),
--- redbuttegarden/plants/views.py
+++ redbuttegarden/plants/views.py
@@ -2,6 +2,7 @@ from rest_framework import generics, status
 from rest_framework.decorators import api_view
 from rest_framework.parsers import JSONParser
 from rest_framework.response import Response
+from rest_framework.views import APIView
 
 from .models import Family, Species, Collection
 from .serializers import FamilySerializer, SpeciesSerializer, CollectionSerializer
@@ -15,17 +16,16 @@ class SpeciesDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Species.objects.all()
     serializer_class = SpeciesSerializer
 
-@api_view(['GET', 'POST'])
-def collection_list(request, format=None):
+class CollectionList(APIView):
     """
     List 100 most recently created collections, or create new collections.
     """
-    if request.method == 'GET':
+    def get(self, request, format=None):
         collections = Collection.objects.all().order_by('-id')[:100]
         serializer = CollectionSerializer(collections, many=True)
         return Response(serializer.data)
 
-    elif request.method == 'POST':
+    def post(self, request, format=None):
         data = JSONParser().parse(request)
         serializer = CollectionSerializer(data=data)
         if serializer.is_valid():
@@ -33,27 +33,30 @@ def collection_list(request, format=None):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-@api_view(['GET', 'PUT', 'DELETE'])
-def collection_detail(request, pk, format=None):
+class CollectionDetail(APIView):
     """
     Retrieve, update or delete a living plant collection.
     """
-    try:
-        collection = Collection.objects.get(pk=pk)
-    except Collection.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
+    def get_object(self, pk):
+        try:
+            return Collection.objects.get(pk=pk)
+        except Collection.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
 
-    if request.method == 'GET':
+    def get(self, request, pk, format=None):
+        collection = self.get_object(pk)
         serializer = CollectionSerializer(collection)
         return Response(serializer.data)
 
-    elif request.method == 'PUT':
+    def put(self, request, pk, format=None):
+        collection = self.get_object(pk)
         serializer = CollectionSerializer(collection, data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    elif request.method == 'DELETE':
+    def delete(self, request, pk, format=None):
+        collection = self.get_object(pk)
         collection.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
