af148d2c44983c509c8fad8fe5b4a1ad453644f0
Avery Uslaner <avery.uslaner@redbutte.utah.edu>
2021-06-29 11:40:01 -0600

    Refactors collections views using DRF generic view
    Signed-off-by: Avery Uslaner <avery.uslaner@redbutte.utah.edu>

--- redbuttegarden/plants/views.py
+++ redbuttegarden/plants/views.py
@@ -1,6 +1,4 @@
-from rest_framework import generics, status, mixins
-from rest_framework.response import Response
-from rest_framework.views import APIView
+from rest_framework import generics
 
 from .models import Family, Species, Collection
 from .serializers import FamilySerializer, SpeciesSerializer, CollectionSerializer
@@ -14,45 +12,17 @@ class SpeciesDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Species.objects.all()
     serializer_class = SpeciesSerializer
 
-class CollectionList(mixins.ListModelMixin,
-                     mixins.CreateModelMixin,
-                     generics.GenericAPIView):
+class CollectionList(generics.ListCreateAPIView):
     """
     List 100 most recently created collections, or create new collections.
     """
     queryset = Collection.objects.all().order_by('-id')[:100]
     serializer_class = CollectionSerializer
 
-    def get(self, request, *args, **kwargs):
-        return self.list(request, *args, **kwargs)
 
-    def post(self, request, *args, **kwargs):
-        return self.create(request, *args, **kwargs)
-
-class CollectionDetail(APIView):
+class CollectionDetail(generics.RetrieveUpdateDestroyAPIView):
     """
     Retrieve, update or delete a living plant collection.
     """
-    def get_object(self, pk):
-        try:
-            return Collection.objects.get(pk=pk)
-        except Collection.DoesNotExist:
-            return Response(status=status.HTTP_404_NOT_FOUND)
-
-    def get(self, request, pk, format=None):
-        collection = self.get_object(pk)
-        serializer = CollectionSerializer(collection)
-        return Response(serializer.data)
-
-    def put(self, request, pk, format=None):
-        collection = self.get_object(pk)
-        serializer = CollectionSerializer(collection, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def delete(self, request, pk, format=None):
-        collection = self.get_object(pk)
-        collection.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+    queryset = Collection.objects.all()
+    serializer_class = CollectionSerializer
