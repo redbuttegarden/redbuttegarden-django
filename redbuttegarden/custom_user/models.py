7654a65406a493a6626fdb3cd877fd03a7f6055b
Avery Uslaner <avery.uslaner@redbutte.utah.edu>
2021-07-08 17:11:50 -0600

    Removes PlantAPI Group model
    Signed-off-by: Avery Uslaner <avery.uslaner@redbutte.utah.edu>

--- redbuttegarden/custom_user/models.py
+++ redbuttegarden/custom_user/models.py
@@ -10,13 +10,3 @@ class User(AbstractUser):
 
     def __str__(self):
         return f'{self.first_name} {self.last_name} ({self.username})'
-
-
-class PlantAPIGroup(models.Model):
-    name = models.CharField(verbose_name="Group", max_length=255)
-    description = models.TextField(verbose_name="Description", blank=True, null=True)
-    roles = models.ManyToManyField(Group, verbose_name="Role", related_name="apigroups", blank=True)
-
-class Meta:
-    verbose_name = "BRAHMS API group"
-    verbose_name_plural = "BRAHMS API groups"
