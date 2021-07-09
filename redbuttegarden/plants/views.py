c68fdae6d657f994db04a1ebcf46d8da103c7840
Avery Uslaner <avery.uslaner@redbutte.utah.edu>
2021-07-09 10:37:15 -0600

    API Based views restricted to API group membership
    Signed-off-by: Avery Uslaner <avery.uslaner@redbutte.utah.edu>

--- redbuttegarden/plants/views.py
+++ redbuttegarden/plants/views.py
@@ -1,4 +1,5 @@
-from rest_framework import generics, permissions, viewsets
+from django.contrib.auth.mixins import UserPassesTestMixin
+from rest_framework import generics, viewsets
 
 from django.shortcuts import render, get_object_or_404
 
@@ -6,56 +7,52 @@ from .models import Family, Genus, Species, Collection, Location
 from .serializers import FamilySerializer, SpeciesSerializer, CollectionSerializer, GenusSerializer, LocationSerializer
 
 
-class FamilyViewSet(viewsets.ModelViewSet):
+class TestAPIGroupMembership(UserPassesTestMixin):
+    def test_func(self):
+        return self.request.user.groups.filter(name='API').exists()
+
+class FamilyViewSet(TestAPIGroupMembership, viewsets.ModelViewSet):
     """
     List, create, retrieve, update or delete families.
     """
     queryset = Family.objects.all()
     serializer_class = FamilySerializer
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
-class GenusViewSet(viewsets.ModelViewSet):
+class GenusViewSet(TestAPIGroupMembership, viewsets.ModelViewSet):
     """
     List, create, retrieve, update or delete genera.
     """
     queryset = Genus.objects.all()
     serializer_class = GenusSerializer
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
-class SpeciesList(generics.ListCreateAPIView):
+class SpeciesList(TestAPIGroupMembership, generics.ListCreateAPIView):
     queryset = Species.objects.all()
     serializer_class = SpeciesSerializer
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
-class SpeciesDetail(generics.RetrieveUpdateDestroyAPIView):
+class SpeciesDetail(TestAPIGroupMembership, generics.RetrieveUpdateDestroyAPIView):
     queryset = Species.objects.all()
     serializer_class = SpeciesSerializer
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
-class LocationViewSet(viewsets.ModelViewSet):
+class LocationViewSet(TestAPIGroupMembership, viewsets.ModelViewSet):
     """
     List, create, retrieve, update or delete locations.
     """
     queryset = Location.objects.all()
     serializer_class = LocationSerializer
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
-class CollectionList(generics.ListCreateAPIView):
+class CollectionList(TestAPIGroupMembership, generics.ListCreateAPIView):
     """
     List 100 most recently created collections, or create new collections.
     """
     queryset = Collection.objects.all().order_by('-id')[:100]
     serializer_class = CollectionSerializer
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
-
-class CollectionDetail(generics.RetrieveUpdateDestroyAPIView):
+class CollectionDetail(TestAPIGroupMembership, generics.RetrieveUpdateDestroyAPIView):
     """
     Retrieve, update or delete a living plant collection.
     """
     queryset = Collection.objects.all()
     serializer_class = CollectionSerializer
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
 def plant_map_view(request):
     return render(request, 'plants/collection_map.html')
