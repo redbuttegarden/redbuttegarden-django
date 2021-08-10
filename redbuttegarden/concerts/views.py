9798d2b18ae4d2a2940b77bab0fc7015c24546b9
Avery Uslaner <avery.uslaner@redbutte.utah.edu>
2021-08-10 16:43:51 -0600

    Adds "Thank You" page for After Etix Ticket Purchase
    Signed-off-by: Avery Uslaner <avery.uslaner@redbutte.utah.edu>

--- redbuttegarden/concerts/views.py
+++ redbuttegarden/concerts/views.py
@@ -1,5 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 
 def concerts(request):
     return render(request, 'concerts/concert_page.html')
+
+def concert_thank_you(request):
+    referer = request.META.get('HTTP_REFERER')
+    if referer and 'etix.com' in referer:
+        return render(request, 'concerts/concert_thank_you.html')
+    else:
+        return redirect('/')
