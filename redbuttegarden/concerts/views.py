5bef535fddb1cc3be57a97c1fb388da56cc91ca7
Avery Uslaner <avery.uslaner@redbutte.utah.edu>
2021-08-12 09:19:45 -0600

    Adds additional debug info to "thank you" view
    Signed-off-by: Avery Uslaner <avery.uslaner@redbutte.utah.edu>

--- redbuttegarden/concerts/views.py
+++ redbuttegarden/concerts/views.py
@@ -10,6 +10,7 @@ def concerts(request):
 def concert_thank_you(request):
     referer = request.META.get('HTTP_REFERER')
     logger.debug(f'Referer: {referer}')
+    logger.debug(f'HTTP META Dictionary: {request.META}')
     if referer and 'etix.com' in referer:
         return render(request, 'concerts/concert_thank_you.html')
     else:
