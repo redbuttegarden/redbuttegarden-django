03cb21fdbff658fad272e937b24e08701b808ff9
Diego Andino <diego.andino@redbutte.utah.edu>
2021-08-17 11:06:32 -0600

    Non valid URL media fix on IG Request JS

--- redbuttegarden/home/static/js/instafeed-request.js
+++ redbuttegarden/home/static/js/instafeed-request.js
@@ -1,4 +1,4 @@
-const access_token = "EAAHc3xMKegsBAOskyer8GkXjUCMwh1grHvg30lyCOvcOA63ssEcf2nMYytNRYCwIo7EJK46i9C1QT1nQlCMfUlpSA2GCYmVST28162ysrd2akc2bDzrDqtByranFxLsuWxDt1C8qGdAahJWzIWVSgyd2ZBWxJES8ZAtZAgNMb4ZCUi4ZAoyC18cn1xt0Kut3nQQd930tb97FmLIL2SvLgEzZCEj9ft3EcZD"
+const access_token = "EAAHc3xMKegsBAGK7vcWQT6NntJZCmiZAZCZBqUNlN4yomeOhy6sxLu0SRft13fR5Lcps4AeXZCEU2A5YuZCC3tc4BgKbUqkjG0AEfZACpiT9HQjUtyH8ImZBkFO9IgUUjYPaKECkcxOIXNXvBy8xM4bQOAMTv30NGo6JZBK3ERNPtjooat63z2UTJSvmSkRLTihyEYO7Ya3YJvszmkOixX118xubCazKMVDMZD"
 const ig_user = "17841401164963561"
 
 fetch(`https://graph.facebook.com/v11.0/${ig_user}/media?access_token=${access_token}`)
@@ -31,7 +31,10 @@ fetch(`https://graph.facebook.com/v11.0/${ig_user}/media?access_token=${access_t
 
                     let mediaImg = document.createElement('img')
                     mediaImg.id = "media-img"
-                    mediaImg.src = media_url
+                    
+                    if (media_url != "") mediaImg.src = media_url 
+                    else mediaParent.style = "display: none;"
+
                     mediaImg.style = (window.matchMedia("(max-width: 420px) and (max-height: 740px)").matches 
                         ? "width: 150px; height: 150px;" 
                         : "width: 350px; height: 350px;")
