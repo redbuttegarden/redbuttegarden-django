b3a0266ae06001a4cce7bd3d5f8f43dd13b24ad2
Avery Uslaner <avery.uslaner@redbutte.utah.edu>
2021-08-17 13:29:55 -0600

    Removed unused VR Tour view
    Signed-off-by: Avery Uslaner <avery.uslaner@redbutte.utah.edu>

--- redbuttegarden/home/views.py
+++ redbuttegarden/home/views.py
@@ -1,10 +1,5 @@
-from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
 
 def social_media(request):
     return render(request, 'home/social_media.html')
-
-@login_required
-def vr_tours(request):
-    return render(request, 'home/vr_tours.html')
