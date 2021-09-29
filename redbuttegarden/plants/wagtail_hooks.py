6c6032a5e71065f6374cb2055024ae0891616aaa
Avery Uslaner <avery.uslaner@redbutte.utah.edu>
2021-09-29 11:35:56 -0600

    ModelAdmin Optimizations
    Overrode get_queryset method for SpeciesAdmin and CollectionsAdmin to related models would be also be selected, to avoid additional database queries on __str__ method lookups.
    
    Modified IndexView ordering of SpeciesAdmin.
    
    Expanded list_display, search_fields, and list_filter of CollectionsAdmin.
    
    Signed-off-by: Avery Uslaner <avery.uslaner@redbutte.utah.edu>

--- redbuttegarden/plants/wagtail_hooks.py
+++ redbuttegarden/plants/wagtail_hooks.py
@@ -10,16 +10,37 @@ class SpeciesAdmin(ModelAdmin):
     list_display = ('genus', 'name', 'cultivar')
     list_filter = ('genus',)
     search_fields = ('genus__family__name', 'genus__name', 'name', 'cultivar', 'vernacular_name')
-    ordering = ('-genus', 'name')
+    ordering = ('genus', 'name')
+
+    def get_queryset(self, request):
+        qs = self.model._default_manager.select_related(
+            'genus__family'
+        )
+        ordering = self.get_ordering(request)
+        if ordering:
+            qs = qs.order_by(*ordering)
+
+        return qs
 
 
 class CollectionsAdmin(ModelAdmin):
     model = Collection
     menu_label = 'Collections'
     menu_icon = 'pilcrow'
-    list_display = ('species',)
-    list_filter = ('species',)
-    search_fields = ('species',)
+    list_display = ('species', 'plant_id')
+    list_filter = ('species__genus',)
+    search_fields = ('species__genus__family__name', 'species__genus__name', 'species__name', 'plant_id',)
+    ordering = ('species__genus',)
+
+    def get_queryset(self, request):
+        qs = self.model._default_manager.select_related(
+            'species__genus__family'
+        )
+        ordering = self.get_ordering(request)
+        if ordering:
+            qs = qs.order_by(*ordering)
+
+        return qs
 
 
 class PlantGroup(ModelAdminGroup):
