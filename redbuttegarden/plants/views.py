26845190ec7f6a872a4eb471c7c27814b1bf511b
Avery Uslaner <avery.uslaner@redbutte.utah.edu>
2021-10-13 13:53:12 -0600

    Collection detail view change for previous commit
    Signed-off-by: Avery Uslaner <avery.uslaner@redbutte.utah.edu>

--- redbuttegarden/plants/views.py
+++ redbuttegarden/plants/views.py
@@ -219,7 +219,8 @@ def collection_detail(request, collection_id):
     View for displaying detailed info about a single Collection object.
     """
     collection = get_object_or_404(Collection, pk=collection_id)
-    return render(request, 'plants/collection_detail.html', {'collection': collection})
+    return render(request, 'plants/collection_detail.html', {'collection': collection,
+                                                             'full_name': str(collection.species)})
 
 def species_detail(request, species_id):
     """
