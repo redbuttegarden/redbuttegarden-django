cab4201b3cafb505179dffcdbf3be1edb604c071
Avery Uslaner <avery.uslaner@redbutte.utah.edu>
2021-10-13 13:16:05 -0600

    Plant Models Refactoring, Species String, & Species Constraint
    Refactored imports to reference fields from models rather than importing them all separately.
    
    Modified __str__ representation of Species objects to handle a wider variety of name combinations and style them appropriately.
    
    Added a constraint to Species objects to ensure that vernacular_name is not an empty string.
    
    Signed-off-by: Avery Uslaner <avery.uslaner@redbutte.utah.edu>

--- redbuttegarden/plants/models.py
+++ redbuttegarden/plants/models.py
@@ -1,8 +1,7 @@
 from django.contrib.postgres.fields import ArrayField
 from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
-from django.db.models import DecimalField, CharField, ForeignKey, PositiveSmallIntegerField, DateField, DateTimeField, \
-    BooleanField
+from django.db.models.functions import Length
 from django.utils.dates import MONTHS
 from modelcluster.fields import ParentalKey
 from modelcluster.models import ClusterableModel
@@ -10,10 +9,12 @@ from wagtail.admin.edit_handlers import FieldPanel, InlinePanel
 from wagtail.core.models import Orderable
 from wagtail.images.edit_handlers import ImageChooserPanel
 
+models.CharField.register_lookup(Length)
+
 
 class Family(models.Model):
-    name = CharField(max_length=255, unique=True)
-    vernacular_name = CharField(max_length=255, blank=True, null=True)
+    name = models.CharField(max_length=255, unique=True)
+    vernacular_name = models.CharField(max_length=255, blank=True, null=True)
 
     def __repr__(self):
         return ' '.join([self.name])
@@ -22,8 +23,8 @@ class Family(models.Model):
         ordering = ['name']
 
 class Genus(models.Model):
-    family = ForeignKey(Family, on_delete=models.CASCADE)
-    name = CharField(max_length=255, unique=True)
+    family = models.ForeignKey(Family, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255, unique=True)
 
     def __str__(self):
         return ' '.join([self.name, f'({self.family.name})'])
@@ -34,25 +35,26 @@ class Genus(models.Model):
 class Species(ClusterableModel):
     bloom_time_choices = [(v, v) for _, v in MONTHS.items()]
 
-    genus = ForeignKey(Genus, on_delete=models.CASCADE)
-    name = CharField(max_length=255, blank=True, null=True)
-    cultivar = CharField(max_length=255, blank=True, null=True)
-    vernacular_name = CharField(max_length=255)
-    habit = CharField(max_length=255)
-    hardiness = ArrayField(base_field=PositiveSmallIntegerField(validators=[MinValueValidator(1),
+    genus = models.ForeignKey(Genus, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255, blank=True, null=True)
+    cultivar = models.CharField(max_length=255, blank=True, null=True)
+    vernacular_name = models.CharField(max_length=255)
+    habit = models.CharField(max_length=255)
+    hardiness = ArrayField(base_field=models.PositiveSmallIntegerField(validators=[MinValueValidator(1),
                                                                             MaxValueValidator(13)]),
                            size=13, blank=True, null=True)
-    water_regime = CharField(max_length=255, blank=True, null=True)
-    exposure = CharField(max_length=255, blank=True, null=True)
-    bloom_time = ArrayField(base_field=CharField(choices=bloom_time_choices, max_length=255), blank=True, null=True)
-    plant_size = CharField(max_length=255, blank=True, null=True)
-    flower_color = CharField(max_length=255, blank=True, null=True)
-    utah_native = BooleanField(default=False)
-    plant_select = BooleanField(default=False)
-    deer_resist = BooleanField(default=False)
-    rabbit_resist = BooleanField(default=False)
-    bee_friend = BooleanField(default=False)
-    high_elevation = BooleanField(default=False)
+    water_regime = models.CharField(max_length=255, blank=True, null=True)
+    exposure = models.CharField(max_length=255, blank=True, null=True)
+    bloom_time = ArrayField(base_field=models.CharField(choices=bloom_time_choices, max_length=255), blank=True,
+                            null=True)
+    plant_size = models.CharField(max_length=255, blank=True, null=True)
+    flower_color = models.CharField(max_length=255, blank=True, null=True)
+    utah_native = models.BooleanField(default=False)
+    plant_select = models.BooleanField(default=False)
+    deer_resist = models.BooleanField(default=False)
+    rabbit_resist = models.BooleanField(default=False)
+    bee_friend = models.BooleanField(default=False)
+    high_elevation = models.BooleanField(default=False)
 
     panels = [
         InlinePanel('species_images', label='Species Images'),
@@ -76,15 +78,24 @@ class Species(ClusterableModel):
     ]
 
     def __str__(self):
-        if self.cultivar:
+        if self.cultivar and self.name:
             return ' '.join([self.genus.name, self.name, "'" + self.cultivar + "'"])
 
-        return ' '.join([self.genus.name, self.name, self.cultivar])
+        if self.cultivar and not self.name:
+            return ' '.join([self.genus.name, "'" + self.cultivar + "'"])
+
+        if self.name and not self.cultivar:
+            return ' '.join([self.genus.name, self.name])
+
+        return ' '.join([self.genus.name, self.vernacular_name])
 
     class Meta:
         ordering = ['name']
         unique_together = ['genus', 'name', 'cultivar']
         verbose_name_plural = 'species'
+        constraints = [
+            models.CheckConstraint(check=models.Q(vernacular_name__length__gt=0), name='vernacular_name_not_empty')
+        ]
 
 class SpeciesImage(Orderable):
     species = ParentalKey(Species, on_delete=models.CASCADE, related_name='species_images')
@@ -101,27 +112,27 @@ class SpeciesImage(Orderable):
     ]
 
 class Location(models.Model):
-    latitude = DecimalField(max_digits=9, decimal_places=6)
-    longitude = DecimalField(max_digits=9, decimal_places=6)
+    latitude = models.DecimalField(max_digits=9, decimal_places=6)
+    longitude = models.DecimalField(max_digits=9, decimal_places=6)
 
     def __str__(self):
         return ' '.join([str(self.latitude), str(self.longitude)])
 
 class GardenArea(models.Model):
-    area = CharField(max_length=255, blank=True, null=True)
-    name = CharField(max_length=255, blank=True, null=True)
-    code = CharField(max_length=20, blank=True, null=True)
+    area = models.CharField(max_length=255, blank=True, null=True)
+    name = models.CharField(max_length=255, blank=True, null=True)
+    code = models.CharField(max_length=20, blank=True, null=True)
 
 class Collection(models.Model):
     location = models.ForeignKey(Location, on_delete=models.CASCADE)
     garden = models.ForeignKey(GardenArea, on_delete=models.SET_NULL, null=True)
-    species = ForeignKey(Species, on_delete=models.CASCADE)
-    plant_date = DateField(blank=True, null=True)
-    plant_id = CharField(max_length=255, null=True, blank=True)
-    commemoration_category = CharField(max_length=255, null=True, blank=True)
-    commemoration_person = CharField(max_length=255, null=True, blank=True)
-    created_on = DateTimeField(auto_now_add=True)
-    last_modified = DateTimeField(auto_now=True)
+    species = models.ForeignKey(Species, on_delete=models.CASCADE)
+    plant_date = models.DateField(blank=True, null=True)
+    plant_id = models.CharField(max_length=255, null=True, blank=True)
+    commemoration_category = models.CharField(max_length=255, null=True, blank=True)
+    commemoration_person = models.CharField(max_length=255, null=True, blank=True)
+    created_on = models.DateTimeField(auto_now_add=True)
+    last_modified = models.DateTimeField(auto_now=True)
 
     class Meta:
         ordering = ['-created_on']
