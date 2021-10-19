f7e621807b6db3e52695df3153e3965b1d3657d1
Avery Uslaner <avery.uslaner@redbutte.utah.edu>
2021-10-19 11:58:24 -0600

    Rearrange image related tests to avoid interactions between tests
    Signed-off-by: Avery Uslaner <avery.uslaner@redbutte.utah.edu>

--- redbuttegarden/plants/tests/test_api_external.py
+++ redbuttegarden/plants/tests/test_api_external.py
@@ -12,7 +12,7 @@ from plants.models import SpeciesImage, Collection
 from .utils import get_custom_user, get_family, get_genus, get_species
 
 
-class TestImageAPIFromExternalPerspective(APILiveServerTestCase):
+class TestSingleImageAPIFromExternalPerspective(APILiveServerTestCase):
     def setUp(self) -> None:
         self.user = get_custom_user(username='username', password='password')
         Token.objects.create(user=self.user)
@@ -34,21 +34,25 @@ class TestImageAPIFromExternalPerspective(APILiveServerTestCase):
             'Authorization': 'Token ' + token['drf_token']
         })
 
+        self.img_file_obj = BytesIO()
+
+        image_one = Image.new('RGB', size=(1, 1), color=(256, 0, 0))
+        image_one.save(self.img_file_obj, 'jpeg')
+        self.img_file_obj.seek(0)
+        self.img_one = ImageFile(self.img_file_obj, name='1.jpg')
+
+    def tearDown(self) -> None:
+        self.img_file_obj.close()
+
 
     def test_posting_species_image(self):
         url = self.live_server_url + reverse('plants:api-set-species-image', kwargs={'pk': self.species.pk})
-        img_one_file_obj = BytesIO()
-
-        image_one = Image.new('RGB', size=(1, 1), color=(256, 0, 0))
-        image_one.save(img_one_file_obj, 'jpeg')
-        img_one_file_obj.seek(0)
-        img_one = ImageFile(img_one_file_obj, name='1.jpg')
 
         test_copyright_info = 'Example Copyright Info'
 
         self.assertRaises(SpeciesImage.DoesNotExist, SpeciesImage.objects.get, species=self.species)
 
-        r = self.client.post(url, data={'copyright_info': test_copyright_info}, files={'image': img_one})
+        r = self.client.post(url, data={'copyright_info': test_copyright_info}, files={'image': self.img_one})
         self.assertTrue(status.is_success(r.status_code))
 
         species_image = SpeciesImage.objects.get(species=self.species)
