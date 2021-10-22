bc9ef9e4e9a603c10a98905f3404f6a63278d6e2
Avery Uslaner <avery.uslaner@redbutte.utah.edu>
2021-10-22 17:35:02 -0600

    Adds proper styling to display of scientific names
    Signed-off-by: Avery Uslaner <avery.uslaner@redbutte.utah.edu>

---
+++ redbuttegarden/plants/static/js/name_styling.js
@@ -0,0 +1,19 @@
+function style_full_name(name) {
+    let parts = name.split(" ");
+
+    for (let i = 0; i < parts.length; i++) {
+        if (typeof parts[i + 1] != 'undefined' && parts[i][0] === "'") {
+            parts[i] = '</span><span>' + parts[i] + ' ' + parts[i + 1];
+            parts.splice(i + 1, 1);
+        } else if (parts[i][0] === "'" && parts[i][parts[i].length - 1] === "'") {
+            parts[i] = '</span><span>' + parts[i];
+        } else if (parts[i] === 'var.' || parts[i] === 'subsp.' || parts[i] === 'f.') {
+            parts[i] = '</span>' + parts[i] + '<span class="italic">';
+        }
+    }
+
+    parts.splice(0, 0, '<span class="italic">');
+    parts.splice(parts.length, 0, '</span>');
+
+    return parts.join(" ");
+}
