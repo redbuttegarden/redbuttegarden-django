30bd92473d5f6f7ba159f55874f9749635c42a16
Avery Uslaner <avery.uslaner@redbutte.utah.edu>
2021-11-12 13:05:53 -0700

    Added search test to check if "Training" Site Pages are returned
    Signed-off-by: Avery Uslaner <avery.uslaner@redbutte.utah.edu>

---
+++ redbuttegarden/search/tests/test_search.py
@@ -0,0 +1,38 @@
+from pprint import pprint
+
+from django.contrib.auth import get_user_model
+from django.contrib.auth.models import Group, AnonymousUser
+from django.test import TestCase, RequestFactory
+from django.urls import reverse
+from wagtail.core.models import Site, Page
+
+from search.views import search
+
+
+class SearchTestCase(TestCase):
+    def setUp(self) -> None:
+        self.user = get_user_model().objects.create_user('Test User', 'test@email.com', 'password')
+        self.user.groups.add(Group.objects.get(name="Moderators"))
+        self.client.force_login(self.user)
+
+        self.default_site = Site.objects.get(id=1)
+        self.root_page = Page.objects.get(id=1)
+        self.training_root_page = Page(owner=self.user, slug='Test', title='Test')
+        self.root_page.add_sibling(instance=self.training_root_page)
+        self.training_root_page.save_revision().publish()
+        self.training_site = Site(hostname='localhost', port='8000', site_name='Training',
+                                  root_page=self.training_root_page)
+        self.training_site.save()
+
+        self.factory = RequestFactory()
+
+    def test_search_does_not_return_training_pages(self):
+        """
+        Searches should not return results from the Wagtail "Training" Site.
+        """
+        request = self.factory.get(reverse('search'), {'query' : 'Test'})
+        request.user = AnonymousUser()
+
+        response = search(request)
+
+        self.assertNotContains(response, '<h4><a href="http://localhost:8000/">Test</a></h4>')
