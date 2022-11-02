4bfb84df2db4c298b35714416f7f27a735faac06
Avery Uslaner <avery.uslaner@redbutte.utah.edu>
2022-11-02 13:07:22 -0600

    Adds map links and opens a modal to view each type of map

--- redbuttegarden/concerts/static/concerts/js/bootstrap_table_block.js
+++ redbuttegarden/concerts/static/concerts/js/bootstrap_table_block.js
@@ -9,14 +9,31 @@ donor_parking_map.setAttribute("href", "/media/images/donor-permit-parking-map.o
 const concert_club_parking_map = document.createElement("a");
 concert_club_parking_map.setAttribute("href", "/media/images/concert-club-parking-map.original.jpg");
 
+/* Add links to Package Comparison drop-down tables */
 $("#spon-table table td:nth-child(3)").each(function() {
     $(this).wrapInner(donor_seating_map);
 });
 
-$("#spon-table table td:nth-child(6)").each(function() {
+$("#spon-table table td:nth-child(5)").each(function() {
     if (this.innerText.trim() === "Donor Lot") {
         $(this).wrapInner(donor_parking_map);
     } else if (this.innerText.trim() === "Concert Club Lot") {
+        $(this).wrapInner(concert_club_parking_map);
+    }
+});
+
+/* Add links to donor package seating tables */
+$(".donor-package-tables table td:nth-child(5)").each(function() {
+    let text = this.innerText.trim();
+    if (text && !text.includes("Seating Location")) {
+        $(this).wrapInner(donor_seating_map);
+    }
+});
+
+$(".donor-package-tables table td:nth-child(6)").each(function() {
+    if (this.innerText.trim().includes("Donor Lot")) {
+        $(this).wrapInner(donor_parking_map);
+    } else if (this.innerText.trim().includes("Concert Club Lot")) {
         $(this).wrapInner(concert_club_parking_map);
     }
 });
---
+++ redbuttegarden/concerts/static/concerts/js/map_modal.js
@@ -0,0 +1,85 @@
+const mapModal = document.getElementById("mapModal");
+const modalCaption = document.getElementById("caption-display");
+const links = document.getElementsByTagName("a");
+const seatingMapURL = "/media/images/donor-seating-map-2018.original.jpg";
+const donorParkingMapURL = "/media/images/donor-permit-parking-map.original.jpg";
+const concertClubParkingMapURL = "/media/images/concert-club-parking-map.original.jpg";
+
+function createSeatingMapImage() {
+    let map = document.createElement('img');
+    map.src = seatingMapURL;
+    map.alt = 'Amphitheatre Seating Map';
+    return map;
+}
+
+function createDonorParkingMapImage() {
+    let map = document.createElement('img');
+    map.src = donorParkingMapURL;
+    map.alt = 'Donor Parking Map';
+    return map;
+}
+
+function createConcertClubParkingMapImage() {
+    let map = document.createElement('img');
+    map.src = concertClubParkingMapURL;
+    map.alt = 'Concert Club Parking Map';
+    return map;
+}
+
+function addMapImage(map) {
+    mapModal.appendChild(map);
+}
+
+function removeMapImage() {
+    while (mapModal.firstChild) {
+        mapModal.removeChild(mapModal.firstChild);
+    }
+}
+
+function modifyLinks(seatingMap, donorParkingMap, concertClubParkingMap) {
+    for (let i = 0; i < links.length; i++) {
+        let url = links[i].href;
+
+        if (url.includes("seating-map")) {
+            links[i].onclick = function (e) {
+                e.preventDefault();
+                removeMapImage();
+                addMapImage(seatingMap);
+                openModal();
+                let target, text;
+                target = e.target;
+                text = target.textContent || target.innerText;
+                modalCaption.innerText = text;
+            }
+        } else if (url.includes("donor-permit-parking-map")) {
+            links[i].onclick = function (e) {
+                e.preventDefault();
+                removeMapImage();
+                addMapImage(donorParkingMap);
+                openModal();
+                let target, text;
+                target = e.target;
+                text = target.textContent || target.innerText;
+                modalCaption.innerText = text;
+            }
+        } else if (url.includes("concert-club-parking-map")) {
+            links[i].onclick = function (e) {
+                e.preventDefault();
+                removeMapImage();
+                addMapImage(concertClubParkingMap);
+                openModal();
+                let target, text;
+                target = e.target;
+                text = target.textContent || target.innerText;
+                modalCaption.innerText = text;
+            }
+        }
+    }
+}
+
+window.onload = function () {
+    let seatingMap = createSeatingMapImage();
+    let donorParkingMap = createDonorParkingMapImage();
+    let concertClubParkingMap = createConcertClubParkingMapImage();
+    modifyLinks(seatingMap, donorParkingMap, concertClubParkingMap);
+}
