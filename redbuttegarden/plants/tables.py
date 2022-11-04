efe39660bf5e5b1f1e5cd2eb7e1744c8fce8b389
Avery Uslaner <avery.uslaner@redbutte.utah.edu>
2022-11-04 14:59:14 -0600

    Updated accessors to address warnings

--- redbuttegarden/plants/tables.py
+++ redbuttegarden/plants/tables.py
@@ -3,10 +3,10 @@ import django_tables2 as tables
 
 class CollectionTable(tables.Table):
     plant_id = tables.Column(verbose_name='Plant ID', linkify=('plants:collection-detail', [tables.A('pk')]))
-    species = tables.Column(accessor='species.full_name', linkify=('plants:species-detail', [tables.A('species.pk')]))
-    garden_name = tables.Column(verbose_name='Garden Name', accessor='garden.name')
-    garden_area = tables.Column(verbose_name='Garden Area', accessor='garden.area')
-    garden_code = tables.Column(verbose_name='Garden Code', accessor='garden.code')
+    species = tables.Column(accessor='species__full_name', linkify=('plants:species-detail', [tables.A('species__pk')]))
+    garden_name = tables.Column(verbose_name='Garden Name', accessor='garden__name')
+    garden_area = tables.Column(verbose_name='Garden Area', accessor='garden__area')
+    garden_code = tables.Column(verbose_name='Garden Code', accessor='garden__code')
 
     class Meta:
         attrs = {"id": "collection-list-table", "class": "table"}
