e6a9500a9b28a4a417bd6633419619aeca1af0c7
Avery Uslaner <avery.uslaner@redbutte.utah.edu>
2025-02-07 23:09:31 +0000

    Fancier scroll handling for that superior user experience

--- redbuttegarden/home/static/home/js/follow_us_scroll.js
+++ redbuttegarden/home/static/home/js/follow_us_scroll.js
@@ -1,11 +1,41 @@
 const row = document.querySelector('.image-row');
+const images = document.querySelectorAll('.image-row img');
+const leftArrow = document.querySelector('.arrow-left');
+const rightArrow = document.querySelector('.arrow-right');
+let imageIndex = 0;
 
-function scrollSocialRight() {
-    const leftArrow = document.querySelector('.arrow-left');
+rightArrow.addEventListener('click', event => {
+    scrollSocialRight(event);
+});
+
+leftArrow.addEventListener('click', event => {
+    scrollSocialLeft(event);
+});
+
+function scrollSocialRight(event) {
+    determineImageIndex(event.x, event.y);
+    imageIndex++;
+    if (imageIndex > images.length) {
+        imageIndex = images.length;
+    }
     leftArrow.classList.remove('d-none');
-    row.scrollBy({left: 400, behavior: 'smooth'});
+    scrollToImage();
 }
 
-function scrollSocialLeft() {
-    row.scrollBy({left: -400, behavior: 'smooth'});
+function scrollSocialLeft(event) {
+    determineImageIndex(event.x, event.y);
+    imageIndex--;
+    if (imageIndex < 0) {
+        imageIndex = 0;
+    }
+    scrollToImage();
+}
+
+function determineImageIndex(x, y) {
+    const imageUnderClick = document.elementsFromPoint(x, y).find(element => element.tagName === 'IMG');
+    imageIndex = Array.from(images).indexOf(imageUnderClick);
+}
+
+function scrollToImage() {
+    images[imageIndex].scrollIntoView({block: "nearest", inline: "nearest", behavior: "smooth"});
 }
