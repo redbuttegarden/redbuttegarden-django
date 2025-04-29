b988dfe87d106c96b76ef3309a455de98052c9c2
Avery Uslaner <avery.uslaner@redbutte.utah.edu>
2025-04-29 17:36:46 +0000

    Bugfix - Don't use strip() when checking if incoming request contains key

--- redbuttegarden/concerts/views.py
+++ redbuttegarden/concerts/views.py
@@ -313,7 +313,7 @@ def process_ticket_data(request):
             logger.debug(f'Created ConcertDonorClubMember {cdc_member}')
 
         package = None
-        if request.data['package_name'].strip():
+        if request.data['package_name']:
             package, package_created = ConcertDonorClubPackage.objects.get_or_create(name=request.data['package_name'],
                                                                                      defaults={
                                                                                          'year': datetime.datetime.now().year})
