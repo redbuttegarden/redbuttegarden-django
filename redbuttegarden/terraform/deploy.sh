43da0cdb5e0ce8afd70b0b4a597af8a117564a57
Avery Uslaner <avery.uslaner@redbutte.utah.edu>
2025-06-10 19:37:36 +0000

    Remove zappa commands from deploy script since those are better handled manually

--- redbuttegarden/terraform/deploy.sh
+++ redbuttegarden/terraform/deploy.sh
@@ -8,9 +8,7 @@ if [ "$ACTION" == "up" ]; then
   terraform init
   terraform apply -var-file=terraform_${ENV}.tfvars -auto-approve
   python update_zappa_vpc.py $ENV
-  zappa deploy $ENV
 elif [ "$ACTION" == "down" ]; then
-  zappa undeploy $ENV -y
   terraform destroy -var-file=terraform_${ENV}.tfvars -auto-approve
 else
   echo "Usage: ./deploy.sh [environment] [up|down]"
