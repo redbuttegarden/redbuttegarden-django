b44971bb7c47f841dfef3003cc066895b2cb32b7
Avery Uslaner <avery.uslaner@redbutte.utah.edu>
2025-06-10 20:56:22 +0000

    Specify AWS profile to use for terraform commands

--- redbuttegarden/terraform/deploy.sh
+++ redbuttegarden/terraform/deploy.sh
@@ -6,10 +6,10 @@ ACTION=$2
 
 if [ "$ACTION" == "up" ]; then
   terraform init
-  terraform apply -var-file=terraform_${ENV}.tfvars -auto-approve
+  AWS_PROFILE=terraform terraform apply -var-file=terraform_${ENV}.tfvars -auto-approve
   python update_zappa_vpc.py $ENV
 elif [ "$ACTION" == "down" ]; then
-  terraform destroy -var-file=terraform_${ENV}.tfvars -auto-approve
+  AWS_PROFILE=terraform terraform destroy -var-file=terraform_${ENV}.tfvars -auto-approve
 else
   echo "Usage: ./deploy.sh [environment] [up|down]"
 fi
