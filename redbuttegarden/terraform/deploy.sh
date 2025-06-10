d9822e35fee94257fb30ebbde99f1ba31f48ebf3
Avery Uslaner <avery.uslaner@redbutte.utah.edu>
2025-06-10 18:47:33 +0000

    Remove chdir flag in terraform commands

--- redbuttegarden/terraform/deploy.sh
+++ redbuttegarden/terraform/deploy.sh
@@ -5,13 +5,13 @@ ENV=$1
 ACTION=$2
 
 if [ "$ACTION" == "up" ]; then
-  terraform -chdir=terraform init
-  terraform -chdir=terraform apply -var-file=terraform_${ENV}.tfvars -auto-approve
+  terraform init
+  terraform apply -var-file=terraform_${ENV}.tfvars -auto-approve
   python update_zappa_vpc.py $ENV
   zappa deploy $ENV
 elif [ "$ACTION" == "down" ]; then
   zappa undeploy $ENV -y
-  terraform -chdir=terraform destroy -var-file=terraform_${ENV}.tfvars -auto-approve
+  terraform destroy -var-file=terraform_${ENV}.tfvars -auto-approve
 else
   echo "Usage: ./deploy.sh [environment] [up|down]"
 fi
