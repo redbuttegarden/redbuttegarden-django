5bbb260335051b51717c86b8a1b0c4cf4a946f33
Avery Uslaner <avery.uslaner@redbutte.utah.edu>
2025-07-05 16:27:18 +0000

    Add custom_domain property to custom file storage classes

--- redbuttegarden/home/custom_storages.py
+++ redbuttegarden/home/custom_storages.py
@@ -2,16 +2,13 @@ from django.conf import settings
 from storages.backends.s3boto3 import S3Boto3Storage
 
 
-class SecurityTokenWorkaroundS3Boto3Storage(S3Boto3Storage):
-    def _get_security_token(self):
-        return None
-
-
-class MediaStorage(SecurityTokenWorkaroundS3Boto3Storage):
+class MediaStorage(S3Boto3Storage):
     bucket_name = settings.STATIC_BUCKET
     location = 'media'
+    custom_domain = 'staging.redbuttegarden.org'
 
 
-class StaticStorage(SecurityTokenWorkaroundS3Boto3Storage):
-    bucket_name = settings.MEDIA_BUCKET
+class StaticStorage(S3Boto3Storage):
+    bucket_name = settings.STATIC_BUCKET
     location = 'static'
+    custom_domain = 'staging.redbuttegarden.org'
