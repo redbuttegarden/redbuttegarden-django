7204f0dc6a4cb55e0dd618ea579b8413a2368c97
Avery Uslaner <avery.uslaner@redbutte.utah.edu>
2025-07-05 22:48:39 +0000

    Use ManifestFilesMixin for static files and specify different file overwrite strategies between static and media files

--- redbuttegarden/home/custom_storages.py
+++ redbuttegarden/home/custom_storages.py
@@ -1,12 +1,21 @@
 from django.conf import settings
+from django.contrib.staticfiles.storage import ManifestFilesMixin
 from storages.backends.s3boto3 import S3Boto3Storage
 
 
 class MediaStorage(S3Boto3Storage):
+    """
+    Storage for media files (user uploads), no manifest or cache-busting.
+    """
     bucket_name = settings.STATIC_BUCKET
     location = 'media'
+    file_overwrite = False
 
 
-class StaticStorage(S3Boto3Storage):
+class StaticStorage(ManifestFilesMixin, S3Boto3Storage):
+    """
+    Combines ManifestStaticFilesStorage (for cache-busting) with S3Boto3Storage.
+    """
     bucket_name = settings.STATIC_BUCKET
     location = 'static'
+    file_overwrite = True
