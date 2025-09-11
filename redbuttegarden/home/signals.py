54cfd72e9f4cc676d98035b7bb3be82b77874809
Avery Uslaner <avery.uslaner@redbutte.utah.edu>
2025-09-11 15:51:24 +0000

    Invalidate cache of image url when saved

--- redbuttegarden/home/signals.py
+++ redbuttegarden/home/signals.py
@@ -1,4 +1,4 @@
-from wagtail.documents.signals import document_saved
+from wagtail.documents.signals import document_saved, image_saved
 from wagtail.contrib.frontend_cache.utils import purge_url
 from django.dispatch import receiver
 
@@ -10,3 +10,12 @@ def invalidate_document_cache(sender, instance, **kwargs):
 
     # Purge the cache for this URL
     purge_url(doc_url)
+
+
+@receiver(image_saved)
+def invalidate_image_cache(sender, instance, **kwargs):
+    # Get the image's URL
+    image_url = instance.file.url
+
+    # Purge the cache for this URL
+    purge_url(image_url)
