e27994a9246e33e6e8ba0931576b0ba1b55203ed
Avery Uslaner <avery.uslaner@redbutte.utah.edu>
2025-09-11 16:17:11 +0000

    Fix signals to use proper imports and methods

--- redbuttegarden/home/signals.py
+++ redbuttegarden/home/signals.py
@@ -1,21 +1,27 @@
-from wagtail.documents.signals import document_saved, image_saved
-from wagtail.contrib.frontend_cache.utils import purge_url
+from django.db.models.signals import post_save
 from django.dispatch import receiver
+from wagtail.contrib.frontend_cache.utils import PurgeBatch
+from wagtail.documents.models import Document
+from wagtail.images.models import Image
 
 
-@receiver(document_saved)
+@receiver(post_save, sender=Document)
 def invalidate_document_cache(sender, instance, **kwargs):
     # Get the document's URL
     doc_url = instance.url
 
     # Purge the cache for this URL
-    purge_url(doc_url)
+    batch = PurgeBatch()
+    batch.add_url(doc_url)
+    batch.purge()
 
 
-@receiver(image_saved)
+@receiver(post_save, sender=Image)
 def invalidate_image_cache(sender, instance, **kwargs):
     # Get the image's URL
     image_url = instance.file.url
 
     # Purge the cache for this URL
-    purge_url(image_url)
+    batch = PurgeBatch()
+    batch.add_url(image_url)
+    batch.purge()
