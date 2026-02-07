b71cb8389db6c091f5c45452b78b7453231cfaef
Avery Uslaner <avery.uslaner@redbutte.utah.edu>
2026-02-07 19:10:43 +0000

    Add additional styling options for PricingCardBlock

--- redbuttegarden/memberships/wagtail_hooks.py
+++ redbuttegarden/memberships/wagtail_hooks.py
@@ -1,6 +1,9 @@
+from django.templatetags.static import static
+from django.utils.html import format_html
 from wagtail.admin.ui.tables import UpdatedAtColumn
 from wagtail.snippets.models import register_snippet
 from wagtail.snippets.views.snippets import SnippetViewSet
+from wagtail import hooks
 
 from .models import MembershipLevel
 
@@ -8,13 +11,25 @@ from .models import MembershipLevel
 class MembershipLevelViewSet(SnippetViewSet):
     model = MembershipLevel
     icon = "user"
-    list_display = ["name", "cardholders_included", "admissions_allowed", "member_sale_ticket_allowance", "price", UpdatedAtColumn()]
+    list_display = [
+        "name",
+        "cardholders_included",
+        "admissions_allowed",
+        "member_sale_ticket_allowance",
+        "price",
+        UpdatedAtColumn(),
+    ]
     list_per_page = 50
     copy_view_enabled = False
     inspect_view_enabled = False
     admin_url_namespace = "membershiplevel_views"
     base_url_path = "internal/membershiplevel"
-    list_filter = {"cardholders_included": ["exact"], "admissions_allowed": ["exact"], "member_sale_ticket_allowance": ["exact"], "name": ["icontains"]}
+    list_filter = {
+        "cardholders_included": ["exact"],
+        "admissions_allowed": ["exact"],
+        "member_sale_ticket_allowance": ["exact"],
+        "name": ["icontains"],
+    }
 
 
 register_snippet(MembershipLevelViewSet)
