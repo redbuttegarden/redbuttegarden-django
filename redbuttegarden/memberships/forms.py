a0a8ba1ccf27aa33cbc58b3570d2b0d830be9b8c
Avery Uslaner <avery.uslaner@redbutte.utah.edu>
2026-02-11 22:20:12 +0000

    Update label for admissions, set max to 8

--- redbuttegarden/memberships/forms.py
+++ redbuttegarden/memberships/forms.py
@@ -8,18 +8,25 @@ class MembershipSelectorForm(forms.Form):
         initial=1,
         label="Cardholders",
         widget=forms.NumberInput(
-            attrs={"class": "form-control", "min": "1", "aria-label": "Cardholders"}
+            attrs={
+                "class": "form-control",
+                "min": "1",
+                "max": "3",
+                "aria-label": "Cardholders",
+            }
         ),
     )
 
     admissions = forms.IntegerField(
         min_value=1,
+        max_value=8,
         initial=2,
-        label="Admissions per visit",
+        label="Additional Admission Guest Entry",
         widget=forms.NumberInput(
             attrs={
                 "class": "form-control",
                 "min": "1",
+                "max": "8",
                 "aria-label": "Admissions per visit",
             }
         ),
